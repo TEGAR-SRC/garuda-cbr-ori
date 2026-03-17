fa5239d38922057a61426daa99b4c0058043feff
TEGAR-AJA <tegararrahman17@gmail.com>
2026-03-17 23:42:04 +0700

    Fix base_url protocol for HTTPS support

--- application/config/config.php
+++ application/config/config.php
@@ -25,8 +25,8 @@ date_default_timezone_set('Asia/Jakarta');
 |
 */
 
-// $config['base_url'] = '';
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 
 
